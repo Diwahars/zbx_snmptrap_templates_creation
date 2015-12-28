38f48b0ccfd532eb63e8ddf9907ab2e27110fbb0
Zubair AHMED <zubayr.a@gmail.com>
2015-12-28 15:31:28 +0530

    change.

--- mib_basic_processing/snmp_trap_csv_gen.py
+++ mib_basic_processing/snmp_trap_csv_gen.py
@@ -1,4 +1,4 @@
-import mib_trap_processing
+import mib_trap_processing_snmpv1
 
 file_processing_list = [{'file_path': 'mib_data/hp_sim_nsc_nsk/',
                          'csv_name': 'output/output_csv_hp_sim.csv'},
@@ -11,6 +11,6 @@ file_processing_list = [{'file_path': 'mib_data/hp_sim_nsc_nsk/',
 
 for file_to_process in file_processing_list:
     print file_to_process
-    mib_trap_processing.creating_file(file_to_process['csv_name'],
-        mib_trap_processing.file_processing(file_to_process['file_path']))
+    mib_trap_processing_snmpv1.creating_file(file_to_process['csv_name'],
+        mib_trap_processing_snmpv1.file_processing(file_to_process['file_path']))
 
