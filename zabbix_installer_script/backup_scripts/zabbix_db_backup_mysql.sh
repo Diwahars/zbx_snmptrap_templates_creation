7ede4833efb2be6529aa672bbfc23841fb4f5f5c
Zubair AHMED <zubayr.a@gmail.com>
2016-01-06 10:42:18 +0530

    init commit.

---
+++ zabbix_installer_script/backup_scripts/zabbix_db_backup_mysql.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+USERDB='zabbix'
+PASSWD='zabbix'
+BASE='zabbix'
+
+USER='globetouch'
+NMS_SERVER_NAME=`uname -n | cut -d'-' -f1`
+
+
+BACKUP_DIR=/home/${USER}/backup_zabbix_db
+DATE=`date "+%Y_%m_%d_%H_%M"`
+PREV=`date -d '7 days ago' "+%Y_%m_%d"`
+
+FILE_NAME=${NMS_SERVER_NAME}_DBBACKUP_${DATE}.sql
+FILE_NAME_TGZ=${FILE_NAME}.tgz
+
+backup_db()
+{
+
+    cd $BACKUP_DIR
+
+    echo "Creating Backup - Using mysqldump."
+    mysqldump -u$USERDB -p$PASSWD ${BASE} > ${FILE_NAME}
+
+}
+
+start_service()
+{
+    # Start Zabbix Server.
+    echo "Backup Complete - Starting Server Services."
+    service zabbix-server start
+    service zabbix-server status
+}
+
+create_archive()
+{
+    cd $BACKUP_DIR
+
+    echo "Creating Archive."
+    tar cvzf ${FILE_NAME_TGZ} ${FILE_NAME}
+
+    echo "Changing owner to user from root."
+    chown $USER:$USER ${FILE_NAME}.tgz
+
+    echo "Removing Unarchived Backup."
+    rm $BACKUP_DIR/${FILE_NAME}
+
+    echo "Removing Archives older than 7 days."
+    rm $BACKUP_DIR/${NMS_SERVER_NAME}_DBBACKUP_$PREV*
+
+    cd -
+}
+
+
+# Check if directory exsists.
+if [ ! -d $BACKUP_DIR ];
+then
+    mkdir -p $BACKUP_DIR
+fi
+
+# Stop all server services.
+service zabbix-server stop
+
+# Take a DB backup
+backup_db
+
+# Starting Server Services.
+start_service
+
+# Creating Archives.
+create_archive
---
+++ zabbix_installer_script/backup_scripts/zabbix_db_backup_postgres.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+# !!!!!! IMPORTANT !!!!!! 
+#
+# To have the script run without the password,
+# we need to have the `.pgpass` file in the $HOME of the `postgres` user.
+#
+# Format of the file is a below. (`:` seperated values)
+# 
+# <postgres_server_ip>:<postgres_port>:<db_name>:<db_user>:<db_user_password>
+# 
+# Example: one database in one line. (Set file permissions to 0600)
+# localhost:5432:zabbix_db_name:zabbix_user_name:zabbix_password
+#
+
+
+USERDB='zabbix'
+BASE='zabbix'
+
+USER='postgres'
+NMS_SERVER_NAME=`uname -n | cut -d'-' -f1`
+
+
+BACKUP_DIR=/tmp/backup_zabbix_db
+DATE=`date "+%Y_%m_%d_%H_%M"`
+PREV=`date -d '7 days ago' "+%Y_%m_%d"`
+
+FILE_NAME=${NMS_SERVER_NAME}_DBBACKUP_${DATE}.dump
+FILE_NAME_TGZ=${FILE_NAME}.tgz
+
+
+backup_postgres_db()
+{
+	# -------------------
+	# Example Commands
+	# -------------------
+	
+		# Passing `--file` to store the file.
+		#
+		#  sudo -u postgres pg_dump --host localhost --port 5432 \
+		#  			--username zabbix --format custom --blobs --verbos \
+		#			--file /tmp/backup_db/zbx.dump zabbix
+
+		
+		#  Redirecting file using `>`.
+		#
+		#  sudo -u postgres pg_dump --host localhost --port 5432 \
+		#  			--username zabbix --format custom --blobs --verbos \
+		#			zabbix > /tmp/backup_db/zbx.dump zabbix
+
+	# ----------------
+	# TAKING BACKUP
+	# ----------------
+	
+	/usr/bin/pg_dump --host localhost --port 5432 \
+		--username $USERDB --format custom --blobs --verbos \
+		--file ${BACKUP_DIR}/${FILE_NAME} ${BASE}
+		
+}
+
+
+start_service()
+{
+    # Start Zabbix Server.
+    echo "Backup Complete - Starting Server Services."
+    service zabbix-server start
+    service zabbix-server status
+}
+
+create_archive()
+{
+    cd $BACKUP_DIR
+
+    echo "Creating Archive."
+    tar cvzf ${FILE_NAME_TGZ} ${FILE_NAME}
+
+    echo "Changing owner to user from root."
+    chown $USER:$USER ${FILE_NAME}.tgz
+
+    echo "Removing Unarchived Backup."
+    rm $BACKUP_DIR/${FILE_NAME}
+
+    echo "Removing Archives older than 7 days."
+    rm $BACKUP_DIR/${NMS_SERVER_NAME}_DBBACKUP_$PREV*
+
+    cd -
+}
+
+
+# Check if directory exsists.
+if [ ! -d $BACKUP_DIR ];
+then
+    mkdir -p $BACKUP_DIR
+fi
+
+# Take a DB backup
+backup_postgres_db
+
+# Creating Archives.
+create_archive
